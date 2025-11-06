d52a038d9cbae4673de48cc17b38aaeef68f7b8e
yes-2875 <mirzafarrukh97@gmail.com>
2025-11-06 10:49:44 -0500

    Error message UI created on website, now for JS validation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+/* ----- Show Details button functionality in project cards ----- */
 const showDetailsList = document.getElementsByClassName("showDetails");
 
 function showDetails(event) {
@@ -6,13 +8,23 @@ function showDetails(event) {
     
     if (detailsElement.getAttribute("class") === "extraDetails hidden") {
         detailsElement.setAttribute("class", "extraDetails");
+        
+        // Setting the innerText variable replaces the entire innerHTML for some reason, so I'm doing this.
         btn.innerHTML = "<img src=\"images/iconoir_eye-solid.svg\"> Hide Details";
     } else {
         detailsElement.setAttribute("class", "extraDetails hidden");
+        
+        // Setting the innerText variable replaces the entire innerHTML for some reason, so I'm doing this.
         btn.innerHTML = "<img src=\"images/iconoir_eye-solid.svg\"> Show Details";
     }
 }
 
+// Add the showDetails function to every "Show Details" button on the website on click
 for (let button of showDetailsList) {
     button.addEventListener("click", showDetails);
 }
+
+const form = document.querySelector("form");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
